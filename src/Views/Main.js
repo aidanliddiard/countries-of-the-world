f902ca770557eea3957276b1cd3bad1a4c642c92
aidanliddiard <aidanliddiard@gmail.com>
2022-03-15 15:56:57 -0700

    supabase call working

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import CountryCard from '../Components/CountryCard';
+import { fetchCountry } from '../services/country';
 
 export default function Main() {
-  return <div>Maibl bla blan</div>;
+  //const [country, getCountry] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchCountry();
+      console.log(resp);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <CountryCard />
+    </div>
+  );
 }
