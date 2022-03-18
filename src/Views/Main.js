a786c6e3a3358f382042f0e06be98202f34a70ca
aidanliddiard <aidanliddiard@gmail.com>
2022-03-18 10:51:37 -0700

    loader working

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -8,12 +8,14 @@ export default function Main() {
   const [countries, setCountries] = useState([]);
   const [continent, setContinent] = useState('All');
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState('true');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const resp = await fetchCountry();
         setCountries(resp);
+        setLoading('false');
       } catch (e) {
         setErrorMessage('Ooops... our bad. Please reload the page.');
       }
@@ -26,6 +28,27 @@ export default function Main() {
   const filteredCountries = () => {
     return countries.filter((country) => country.continent === continent || continent === 'All');
   };
+
+  if (loading === 'true')
+    return (
+      <div className="loader">
+        <div className="loader">
+          <div className="loader">
+            <div className="loader">
+              <div className="loader">
+                <div className="loader">
+                  <div className="loader">
+                    <div className="loader">
+                      <div className="loader"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <main>
