d4d8355cc3d973f10e135e49d2ea05537eb65333
aidanliddiard <aidanliddiard@gmail.com>
2022-03-18 09:35:52 -0700

    adding some styling

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import CountryCard from '../Components/CountryCard';
+import CountryCard from '../Components/CountryCard/CountryCard';
 import { fetchCountry } from '../services/country';
+import './Main.css';
 
 export default function Main() {
   const [countries, setCountries] = useState([]);
@@ -13,11 +14,17 @@ export default function Main() {
     fetchData();
   }, []);
 
+  const options = ['All', 'Africa', 'Asia', 'Europe', 'Oceania', 'North America', 'South America'];
+
+  // const dropdownContinents = () => {
+  //   return countries.filter((country) => country.continent === continent || type === 'All');
+  // };
+
   return (
-    <div>
+    <main>
       {countries.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
-    </div>
+    </main>
   );
 }
